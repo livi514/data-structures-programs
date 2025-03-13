49311aa6dcf95f88fbec21d277381066f618c142
livi514 <livia5banyai@gmail.com>
2025-03-13 17:54:46 +0000

    Update binarytree.py

--- binarytree.py
+++ binarytree.py
@@ -1,1 +1,11 @@
+#Task: implement a binary tree with functions for insertion and inorder traversal.
+
+#implement the tree as a list
+tree = [null] #the first element will be Null, to make operations easier later
+
+def add_node(node):
+  '''
+  Function for adding a new node to the tree.
+  '''
+  if 
 
