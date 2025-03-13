132b2448d3ec0b80872c62db5080f922996e6496
livi514 <livia5banyai@gmail.com>
2025-03-13 18:05:18 +0000

    Update binarytree.py

--- binarytree.py
+++ binarytree.py
@@ -7,5 +7,34 @@ def add_node(node):
   '''
   Function for adding a new node to the tree.
   '''
-  if 
+  if len(tree) == 1: #this means there are no nodes in the tree. There is one element in the list as the first element is Null.
+    tree.append(node)
+  else:
+    #add node somehow?
 
+def find_left_child(node):
+  node_index = tree.index(node)
+  left_child_index = node_index*2
+  return tree[left_child_index]
+
+def find_right_child(node):
+  node_index = tree.index(node)
+  right_child_index = node_index*2+1
+  return tree[right_child_index]
+
+def find_parent(node):
+  node_index = tree.index(node)
+  parent_index = node_index//2
+  return tree[parent_index]
+
+def find_root(node):
+  return tree[1]
+
+def is_node_leaf(node):
+  node_index = tree.index(node)
+  num_nodes = len(tree)-1
+  if 2*node_index > num_nodes:
+    return true
+  else:
+    return false
+    
